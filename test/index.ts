5918947364edcbfd5aaad8191ebe8b5d3412a8e4
Ismael Souza Silva <ismael1361@gmail.com>
2023-08-26 21:27:24 -0300

    Modificações

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,144 @@
-import { DataBase, Auth, Storage } from "../src/server";
+//import { DataBase, Auth, Storage } from "../src/server";
+import { SimpleEventEmitter } from "ivipbase-core";
+
+import { MongoClient, ChangeStream, ChangeStreamInsertDocument, ChangeStreamDeleteDocument, ChangeStreamUpdateDocument, Collection } from "mongodb";
+
+class MongoDBChangeStreamEmitter extends SimpleEventEmitter {
+	private client: MongoClient;
+	public readonly collection: Promise<Collection>;
+	private changeStream: Promise<ChangeStream>;
+
+	constructor(uri: string, dbName: string, collectionName: string) {
+		super();
+		this.client = new MongoClient(uri);
+
+		this.collection = this.client
+			.connect()
+			.then(() => {
+				const db = this.client.db(dbName);
+				return db.collection(collectionName);
+			})
+			.catch((err) => {
+				this.emit("error", err);
+				throw err;
+			});
+
+		this.changeStream = this.collection
+			.then((collection) => {
+				return collection.watch();
+			})
+			.catch((err) => {
+				this.emit("error", err);
+				throw err;
+			});
+
+		this.setupChangeStreamListeners();
+
+		Promise.all([this.collection, this.changeStream])
+			.then(() => {
+				this.emit("ready", this);
+			})
+			.catch((error) => {
+				this.emit("error", error);
+			});
+	}
+
+	private setupChangeStreamListeners() {
+		this.changeStream.then((stream) => {
+			stream.on("change", (change) => {
+				let event = "added";
+				let newData: any = null;
+				let beforeData: any = null;
+
+				switch (change.operationType) {
+					case "insert":
+						event = "added";
+						newData = (change as ChangeStreamInsertDocument).fullDocument;
+						break;
+					case "delete":
+						event = "removed";
+						beforeData = (change as ChangeStreamDeleteDocument).fullDocumentBeforeChange;
+						break;
+					case "modify":
+					case "update":
+						event = "mutated";
+						newData = (change as ChangeStreamUpdateDocument).fullDocument;
+						beforeData = (change as ChangeStreamUpdateDocument).fullDocumentBeforeChange;
+						break;
+				}
+
+				this.emit(event, { newData, beforeData });
+				this.emit("change", { newData, beforeData });
+			});
+		});
+	}
+
+	on(event: "added" | "mutated" | "removed" | "change" | "ready" | "error", listener: (data: any) => void): any;
+	on(event: string, listener: (data: any) => void): any {
+		return super.on(event, listener);
+	}
+
+	off(event: "added" | "mutated" | "removed" | "change" | "ready" | "error", listener: (data: any) => void): any;
+	off(event: string, listener: (data: any) => void): any {
+		return super.off(event, listener);
+	}
+
+	close() {
+		this.client.close();
+	}
+}
+
+new MongoDBChangeStreamEmitter("mongodb://manager:9Hq91q5oExU9biOZ7yq98I8P1DU1ge@ivipcoin-api.com:4048", "teste", "forWatch").on("ready", (db) => {
+	const collection = db.collection;
+
+	db.on("change", function (change) {
+		console.log(change);
+	});
+
+	setTimeout(function () {
+		collection.insertOne({ batman: "bruce wayne" }, function (err) {
+			console.log(err);
+		});
+	}, 1000);
+
+	setTimeout(function () {
+		collection.insertOne({ superman: "clark kent" }, function (err) {
+			console.log(err);
+		});
+	}, 2000);
+
+	setTimeout(function () {
+		collection.insertOne({ "wonder-woman": "diana prince" }, function (err) {
+			console.log(err);
+		});
+	}, 3000);
+
+	setTimeout(function () {
+		collection.insertOne({ ironman: "tony stark" }, function (err) {
+			console.log(err);
+		});
+	}, 4000);
+
+	setTimeout(function () {
+		collection.insertOne({ spiderman: "peter parker" }, function (err) {
+			console.log(err);
+		});
+	}, 5000);
+
+	// update existing document
+	setTimeout(function () {
+		collection.updateOne({ ironman: "tony stark" }, { $set: { ironman: "elon musk" } }, function (err) {
+			console.log(err);
+		});
+	}, 6000);
+
+	// delete existing document
+	setTimeout(function () {
+		collection.deleteOne({ spiderman: "peter parker" }, function (err) {
+			console.log(err);
+		});
+	}, 7000);
+});
 
 // const password = "9Hq91q5oExU9biOZ7yq98I8P1DU1ge";
 
